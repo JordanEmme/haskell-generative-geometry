e1eacc0aae1aee1aa82ff86b0528b263e4943a05
Tim Hosgood <thosgood@users.noreply.github.com>
2021-07-16 02:36:57 +0100

    monoids!

--- Euclidean.hs
+++ Euclidean.hs
@@ -7,8 +7,17 @@ distance (Vertex x y z) (Vertex x' y' z') = sqrt $ dx + dy + dz
           dy = (y-y') ** 2
           dz = (z-z') ** 2
 
-addition :: Vertex -> Vertex -> Vertex
-addition (Vertex x y z) (Vertex x' y' z') = Vertex (x + x') (y + y') (z + z)
+-- Let's rewrite addition using *monoids* (not monads!) (https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-Monoid.html)
+-- To make Vertex an instance of the Monoid type class, we must define two things:
+--     mempty :: Vertex
+--     <> :: Vertex -> Vertex -> Vertex
+-- These should satisfy (although this is *not* enforced by the compiler (because Haskell doesn't have dependent types))
+--     mempty <> v = v
+--     v <> mempty = v
+--     (u <> v) <> w = u <> (v <> w)
+instance Monoid Vertex where
+    (Vertex x y z) <> (Vertex x' y' z') = Vertex (x + x') (y + y') (z + z)
+    mempty = Vertex 0 0 0
 
 vertex1 = Vertex 1 2 3 
 vertex2 = Vertex 1 2 3 
