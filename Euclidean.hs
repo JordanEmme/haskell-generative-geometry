8b4a7feecdd11db640282b3372eb0baf708090a7
Tim Hosgood <thosgood@users.noreply.github.com>
2021-07-20 19:24:06 +0100

    triangles

--- Euclidean.hs
+++ Euclidean.hs
@@ -16,21 +16,33 @@ scaleVertex (Vertex x y z) s = Vertex (s * x) (s * y) (s * z)
 
 
 -- Edges
-data HalfEdge = HalfEdge { u :: Vertex, v :: Vertex } deriving (Eq, Read, Show)
+type HalfEdge = (Vertex, Vertex)
 
 -- Tim: rewrite this in a Haskell-y-er way
 edgeLength :: HalfEdge -> Float
-edgeLength (HalfEdge u v) = distance u v
+edgeLength (u, v) = distance u v
 
 -- Tim: rewrite this in a Haskell-y-er way
 mirrorEdge :: HalfEdge -> HalfEdge
-mirrorEdge (HalfEdge u v) = HalfEdge v u
+mirrorEdge (u, v) = (v, u)
 
 splitEdge :: HalfEdge -> (HalfEdge, HalfEdge)
-splitEdge (HalfEdge u v) = (firstHalf, secondHalf)
-    where midpoint = scaleVertex (u <> v) 0.5
-          firstHalf = HalfEdge u midpoint
+splitEdge (u, v) = (firstHalf, secondHalf)
+    where midpoint   = scaleVertex (u <> v) 0.5
+          firstHalf  = HalfEdge u midpoint
           secondHalf = HalfEdge midpoint v
+
+
+-- Triangles
+type Triangle = (HalfEdge, HalfEdge, HalfEdge)
+
+splitTriangle :: Triangle -> HalfEdge -> Maybe (Triangle, Triangle)
+splitTriangle (a, b, c) a = Just ((secondHalf, b, newEdge), (firstHalf, (mirrorEdge newEdge), c))
+     where (firstHalf, secondHalf) = splitEdge a
+           newEdge                 = (snd b, fst secondHalf)
+splitTriangle (a, b, c) b = splitTriangle (b, c, a) b
+splitTriangle (a, b, c) c = splitTriangle (c, a, b) c
+splitTriangle _           = Nothing
 
 
 -- Point clouds
